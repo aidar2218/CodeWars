6496dd7d0f13998f462ced260acce94234d647a5
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-09 17:19:12 +0600

    Here I did the second task on CodeWars

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -3,14 +3,29 @@
 // Constraint:   1 <= month <= 12
 
 
-const quarterOf = (month) => {
-    if (month > 0 && month <= 3) {
-        return 1;
-    } else if (month > 3 && month <= 6) {
-        return 2;
-    } else if (month > 6 && month <= 9) {
-        return 3;
-    } else if (month > 9 && month <= 12) {
-        return 4
+// const quarterOf = (month) => {
+//     if (month > 0 && month <= 3) {
+//         return 1;
+//     } else if (month > 3 && month <= 6) {
+//         return 2;
+//     } else if (month > 6 && month <= 9) {
+//         return 3;
+//     } else if (month > 9 && month <= 12) {
+//         return 4
+//     }
+// }
+////////////////////////////////////////////////////////////////
+
+
+
+// Given an array of integers, return a new array with each value doubled.
+// For example: [1, 2, 3] --> [2, 4, 6]
+
+let arrrr = [2, 6, 3];
+
+function maps(arr){
+    for (i = 0; i < arr.length; i++) {
+        arr[i] = arr[i] * 2; 
     }
+    return arr;
 }
