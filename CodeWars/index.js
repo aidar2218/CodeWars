4a9373b335ae5510d036211d11c89340f5e672e2
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-10 16:44:20 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -63,6 +63,15 @@
 // Write a function that accepts an integer n and a string s as 
 // parameters, and returns a string of s repeated exactly n times.
 
-function repeatStr (n, s) {
-    return s.repeat(n);
+// function repeatStr (n, s) {
+//     return s.repeat(n);
+// }
+//////////////////////////////////////////////////////////////////////
+
+
+// Write a function to split a string and convert it into an 
+// array of words.
+
+function stringToArray(string){
+    return string.split(" ");
 }
