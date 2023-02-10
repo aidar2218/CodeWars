9e94ffc19694163ffc92c548e159c5589d552add
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-10 16:12:51 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -1,6 +1,12 @@
-// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
-// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+// Given a month as an integer from 1 to 12, return to which 
+// quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; 
+// month 6 (June), is part of the second quarter; and month 11 (November), 
+// is part of the fourth quarter.
+
 // Constraint:   1 <= month <= 12
+
 
 
 // const quarterOf = (month) => {
@@ -21,11 +27,31 @@
 // Given an array of integers, return a new array with each value doubled.
 // For example: [1, 2, 3] --> [2, 4, 6]
 
-let arrrr = [2, 6, 3];
+// let arrrr = [2, 6, 3];
 
-function maps(arr){
-    for (i = 0; i < arr.length; i++) {
-        arr[i] = arr[i] * 2; 
+// function maps(arr){
+//     for (i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i] * 2; 
+//     }
+//     return arr;
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+
+// You were camping with your friends far away from home, but when it's 
+// time to go back, you realize that your fuel is running out and the 
+// nearest pump is 50 miles away! You know that on average, your car runs 
+// on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is 
+// possible to get to the pump or not.
+
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    if (distanceToPump > (mpg * fuelLeft)) {
+        return false;
+    } else {
+        return true;
     }
-    return arr;
-}
+};
