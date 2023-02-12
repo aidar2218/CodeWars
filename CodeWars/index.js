554a189181678ca9ef59e5a7fc5c76d292bd5028
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-12 16:01:43 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -99,9 +99,54 @@
 // You can assume that all values are integers. Do not mutate the input 
 // array/list.
 
-function invert(array) {
-    for(let i = 0; i < array.length; i++) {
-        array[i] *= -1;
+// function invert(array) {
+//     for(let i = 0; i < array.length; i++) {
+//         array[i] *= -1;
+//     }
+//     return array;
+// }
+///////////////////////////////////////////////////////////////////
+
+
+
+// Create a function with two arguments that will return an array of the 
+// first n multiples of x.
+// Assume both the given number and the number of times to count will be 
+// positive numbers greater than 0.
+// Return the results as an array or list ( depending on language ).
+
+// function countBy(x, n) {
+//     let z = [];
+//     for (i = 1; i <= n; i++) {
+//         z.push(x * i);
+//     }
+//     return z;
+// }
+///////////////////////////////////////////////////////////////////
+
+
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better 
+// than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate 
+// the average and compare your score!
+// Return True if you're better, else False!
+// Note: Your points are not included in the array of your class's 
+// points. For calculating the average point you may add your point 
+// to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+    let average = 0;
+
+    for (i = 0; i < classPoints.length; i++) {
+        average += classPoints[i];
     }
-    return array;
+    average = average / classPoints.length;
+
+    if (yourPoints > average) {
+        return true;
+    } else {
+        return false;
+    }
 }
