75a5d2e589fc00b437c941695b69e5f01026d352
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-12 14:24:48 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -72,6 +72,21 @@
 // Write a function to split a string and convert it into an 
 // array of words.
 
-function stringToArray(string){
-    return string.split(" ");
+// function stringToArray(string){
+//     return string.split(" ");
+// }
+//////////////////////////////////////////////////////////////////////
+
+
+
+// Given an array of integers as strings and numbers, return the sum of 
+// the array values as if all were numbers.
+// Return your answer as a number.
+
+function sumMix(x) {
+    let result = 0;
+    for (i = 0; i < x.length; i++) {
+        result += Number(x[i]);
+    }
+    return result;
 }
