ed46c6b5f81f23c9ce3aad2a97a3c595a3f2feb3
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-17 19:47:17 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -229,10 +229,35 @@
 // If he doesn't get 10 hoops, return the string 
 // "Keep at it until you get it".
 
-function hoopCount (n) {
-    if (n >= 10) {
-        return "Great, now move on to tricks";
-    } else {
-        return "Keep, at it until you get it";
+// function hoopCount (n) {
+//     if (n >= 10) {
+//         return "Great, now move on to tricks";
+//     } else {
+//         return "Keep, at it until you get it";
+//     }
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+
+// Complete the function so that it finds the average of the three scores 
+// passed to it and returns the letter value associated with that grade.
+// Tested values are all between 0 and 100. Theres is no need to check 
+// for negative values or values greater than 100.
+
+function getGrade (s1, s2, s3) {
+    let result = (s1 + s2 + s3) / 3;
+
+    if (result >= 90 && result <= 100) {
+        return "A";
+    } else if (result >= 80 && result < 90) {
+        return "B";
+    } else if (result >= 70 && result < 80) {
+        return "C";
+    } else if (result >= 60 && result < 70) {
+        return "D";
+    } else if (result >= 0 && result < 60) {
+        return "F";
     }
 }
+
