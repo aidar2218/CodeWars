3c97e66f80cfdaa88ac97747c55b306afb9b2cc9
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-17 18:54:50 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -194,14 +194,25 @@
 // completed projects is minimum 2.
 // 0, in other cases.
 
-function finalGrade (exam, projects) {
-    if (exam >= 90 || projects >= 10) {
-        return 100;
-    } else if (exam >= 75 || projects >= 5) {
-        return 90;
-    } else if (exam >= 50 || projects >= 2) {
-        return 75;
+// function finalGrade (exam, projects) {
+//     if(exam > 90 || projects > 10) return 100;
+//     if(exam > 75 & projects >= 5) return 90;
+//     if(exam > 50 & projects >= 2) return 75;
+//     return 0;
+// }
+
+// console.log(finalGrade(55, 4));
+////////////////////////////////////////////////////////////////
+
+
+
+// Create a function that takes an integer as an argument and returns 
+// "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if (number % 2 == 0) {
+        return "Even";
     } else {
-        return 0;
+        return "Odd"
     }
 }
