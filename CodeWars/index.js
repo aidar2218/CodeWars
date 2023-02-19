ae831f4fb10b19f16cc514db73e0a75451813b4e
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-19 18:28:02 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -387,10 +387,49 @@
 // name + " plays banjo" 
 // name + " does not play banjo"
 
-function areYouPlayingBanjo(name) {
-    if (name[0] == "R" || name[0] == "r") {
-        return name + " plays banjo"
+// function areYouPlayingBanjo(name) {
+//     if (name[0] == "R" || name[0] == "r") {
+//         return name + " plays banjo"
+//     } else {
+//         return name + " does not play banjo"
+//     }
+// }
+////////////////////////////////////////////////////////////////////
+
+
+
+// Implement a function that accepts 3 integer values a, b, c. The 
+// function should return true if a triangle can be built with the sides 
+// of given length and false in any other case.
+// (In this case, all triangles must have surface greater than 0 to be 
+// accepted).
+
+// function isTriangle(a,b,c) {
+//     return a + b > c && a + c > b && b + c > a;
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+
+// After a hard quarter in the office you decide to get some rest on a 
+// vacation. So you will book a flight for you and your girlfriend and 
+// try to leave all the mess behind you.
+// You will need a rental car in order for you to get around in your 
+// vacation. The manager of the car rental makes you some good offers.
+// Every day you rent the car costs $40. If you rent the car for 7 or 
+// more days, you get $50 off your total. Alternatively, if you rent 
+// the car for 3 or more days, you get $20 off your total.
+// Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+    let cost = 40 * d;
+    let result;
+    if (d >= 7) {
+        result = cost - 50;
+    } else if (d >= 3) {
+        result = cost - 20;
     } else {
-        return name + " does not play banjo"
+        return cost;
     }
+    return result;
 }
