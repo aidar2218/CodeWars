448314e4c6bd255a4a5194cfe364c4a9bcab6edc
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-19 17:14:09 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -372,6 +372,25 @@
 // You get given the time in hours and you need to return the number of 
 // litres Nathan will drink, rounded to the smallest value.
 
-function litres(time) {
-    return Math.floor(time * 0.5)
+// function litres(time) {
+//     return Math.floor(time * 0.5)
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are 
+// playing banjo!
+// The function takes a name as its only argument, and returns one of 
+// the following strings:
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+function areYouPlayingBanjo(name) {
+    if (name[0] == "R" || name[0] == "r") {
+        return name + " plays banjo"
+    } else {
+        return name + " does not play banjo"
+    }
 }
