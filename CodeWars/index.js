47cde0f9a940ccf822bbe00e834db6a6ac38bbc3
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-27 20:16:35 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -453,14 +453,69 @@
 // numbers between parentheses are shown only for you to see which 
 // numbers are counted in each case.
 
-function getDivisorsCnt(n){
-    let divisors = 0;
-    for (i = 1; i <= n; i++) {
-      if (n % i == 0) {
-        divisors++;
-      }
-    }
-    return divisors;
-}
+// function getDivisorsCnt(n){
+//     let divisors = 0;
+//     for (i = 1; i <= n; i++) {
+//       if (n % i == 0) {
+//         divisors++;
+//       }
+//     }
+//     return divisors;
+// }
 
-console.log(getDivisorsCnt(10));
+
+
+// Given a non-empty array of integers, return the result of 
+// multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// function grow(x){
+//     let num = 1;
+//     for (i = 0; i < x.length; i++) {
+//         num *= x[i];
+//     }
+//     return num;
+// }
+///////////////////////////////////////////////////////////////////////
+
+
+
+// Jenny is 9 years old. She is the youngest detective in North America. 
+// Jenny is a 3rd grader student, so when a new mission comes up, she 
+// gets a code to decipher in a form of a sticker (with numbers) in her 
+// math notebook and a comment (a sentence) in her writing notebook. All 
+// she needs to do is to figure out one word, from there she already 
+// knows what to do. And here comes your role - you can help Jenny find 
+// out what the word is!
+// In order to find out what the word is, you should use the sticker 
+// (array of 3 numbers) to retrive 3 letters from the comment (string) 
+// that create the word.
+// Each of the numbers in the array refers to the position of a letter 
+// in the string, in increasing order.
+// Spaces are not places, you need the actual letters. No spaces.
+// The returned word should be all lowercase letters.
+// if you can't find one of the letters using the index numbers, return 
+// "No mission today". Jenny would be very sad, but that's life... :(
+// Example: input: [5, 0, 3], "I Love You" output: "ivy" 
+// (0 = "i", 3 = "v", 5 = "y")
+
+function missingWord(nums, str) {
+    let newString = str.split(" ").join("").split("");
+    let newNumber = nums.sort();
+    let resultStrArr = [];
+
+    for (i = 0; i < newString.length; i++) {
+        for (j = 0; j < newNumber.length; j++) {
+            if (i == newNumber[j]) {
+                resultStrArr.push(newString[i]);
+            } else if (newNumber[j] > newString.length) {
+                return "No mission today"
+            }
+
+        }
+    }
+
+    let result = resultStrArr.join("");
+
+    return result.toLowerCase();
+}
