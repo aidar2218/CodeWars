454bae595b50d03392161d17c0c5e1d25cf58431
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-02-27 15:39:12 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -421,15 +421,46 @@
 // the car for 3 or more days, you get $20 off your total.
 // Write a code that gives out the total amount for different days(d).
 
-function rentalCarCost(d) {
-    let cost = 40 * d;
-    let result;
-    if (d >= 7) {
-        result = cost - 50;
-    } else if (d >= 3) {
-        result = cost - 20;
-    } else {
-        return cost;
+// function rentalCarCost(d) {
+//     let cost = 40 * d;
+//     let result;
+//     if (d >= 7) {
+//         result = cost - 50;
+//     } else if (d >= 3) {
+//         result = cost - 20;
+//     } else {
+//         return cost;
+//     }
+//     return result;
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+// function func(num) {
+//     num = num || 3;
+
+//     return num * num;
+// }
+
+// console.log(func());
+///////////////////////////////////////////////////////////////////
+
+
+
+// Count the number of divisors of a positive integer n.
+// Random tests go up to n = 500000.
+// Note you should only return a number, the count of divisors. The 
+// numbers between parentheses are shown only for you to see which 
+// numbers are counted in each case.
+
+function getDivisorsCnt(n){
+    let divisors = 0;
+    for (i = 1; i <= n; i++) {
+      if (n % i == 0) {
+        divisors++;
+      }
     }
-    return result;
+    return divisors;
 }
+
+console.log(getDivisorsCnt(10));
