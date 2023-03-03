bd64392876b37c13492e936bcfa3b931bb161b47
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-03-03 10:21:25 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -600,6 +600,27 @@
 // spaces between each word. Be careful, there shouldn't be a space at 
 // the beginning or the end of the sentence!
 
-function smash (words) {
-    return words.join(" ");
-};
+// function smash (words) {
+//     return words.join(" ");
+// };
+//////////////////////////////////////////////////////////////////////
+
+
+
+// Take an array and remove every second element from the array. Always 
+// keep the first element and start removing with the next element.
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> 
+// ["Keep", "Keep", "Keep", ...]
+// None of the arrays will be empty, so you don't have to worry about 
+// that!
+
+function removeEveryOther(arr){
+    let newArray = [];
+    for (i = 0; i < arr.length; i++) {
+        if (i % 2 == 0) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
