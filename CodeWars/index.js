e2c2fb4cdddf369270c6770e658700bb3702c37a
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-03-03 10:10:26 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -587,6 +587,19 @@
 // Example output:
 // Hello, Mr. Spock
 
-function sayHello(name) {
-    return 'Hello, ' + name;
-}
+// function sayHello(name) {
+//     return 'Hello, ' + name;
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+
+// Write a function that takes an array of words and smashes them 
+// together into a sentence and returns the sentence. You can ignore 
+// any need to sanitize words or add punctuation, but you should add 
+// spaces between each word. Be careful, there shouldn't be a space at 
+// the beginning or the end of the sentence!
+
+function smash (words) {
+    return words.join(" ");
+};
