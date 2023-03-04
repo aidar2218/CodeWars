82ea5c36525e2482ea3781779f21f86c3b55330c
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-03-04 15:33:20 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -833,3 +833,13 @@
 
 // console.log(enough(100, 60, 50));
 //////////////////////////////////////////////////////////////////////
+
+
+
+// You are given two interior angles (in degrees) of a triangle.
+// Write a function to return the 3rd.
+// Note: only positive integers will be tested.
+
+function otherAngle(a, b) {
+    return 180 - (a + b);
+}
