b790ee2a1ed66fbf8a5c690762108da8fe6591bd
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-03-04 15:58:15 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -840,6 +840,26 @@
 // Write a function to return the 3rd.
 // Note: only positive integers will be tested.
 
-function otherAngle(a, b) {
-    return 180 - (a + b);
-}
+// function otherAngle(a, b) {
+//     return 180 - (a + b);
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+
+// Let's play! You have to return which player won! In case of a draw 
+// return Draw!.
+// Examples(Input1, Input2 --> Output):
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+    if (p1 == "scissors" && p2 == "paper" || p1 == "paper" && p2 == "rock" || p1 == "rock" && p2 == "scissors") {
+        return "Player 1 won!";
+    } else if (p1 == "scissors" && p2 == "scissors" || p1 == "paper" && p2 == "paper" || p1 == "rock" && p2 == "rock") {
+        return "Draw!";
+    } else {
+        return "Player 2 won!";
+    }
+};
