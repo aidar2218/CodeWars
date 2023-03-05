a9a102f7b30f4ee16b5d9b62588cc2f3dcd91eeb
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-03-05 19:25:02 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -1071,8 +1071,25 @@
 // Use variables to find the sum of the goals Messi scored in 3 
 // competitions
 
-var laLigaGoals = 43;
-var championsLeagueGoals = 10;
-var copaDelReyGoals = 5;
+// var laLigaGoals = 43;
+// var championsLeagueGoals = 10;
+// var copaDelReyGoals = 5;
 
-var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+// var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+//////////////////////////////////////////////////////////////////////
+
+
+
+// Given an array of integers your solution should find the smallest 
+// integer.
+// For example:
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied 
+// array will not be empty.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min.apply(null, args);
+    }
+}
