a18d093e66c8fcf8da82cb355abe84fae1911a34
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-03-19 21:19:18 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -1088,8 +1088,32 @@
 // You can assume, for the purpose of this kata, that the supplied 
 // array will not be empty.
 
-class SmallestIntegerFinder {
-    findSmallestInt(args) {
-        return Math.min.apply(null, args);
+// class SmallestIntegerFinder {
+//     findSmallestInt(args) {
+//         return Math.min.apply(null, args);
+//     }
+// }
+/////////////////////////////////////////////////////////////////////
+
+
+
+// Write function bmi that calculates body mass index (bmi = weight / 
+// height2).
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+    let result = weight / (height * height);
+    if (result <= 18.5) {
+        return "Underweight";
+    } else if (result <= 25) {
+        return "Normal";
+    } else if (result <= 30) {
+        return "Overweight";
+    } else if (result > 30) {
+        return "Obese";
     }
 }
+
