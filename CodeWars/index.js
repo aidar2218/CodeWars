051509ccaa7652b45462b303ac76c5e27b015793
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-07-05 15:39:36 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -1193,12 +1193,40 @@
 // "found the needle at position 5"
 
 
-function findNeedle(haystack) {
-    let position = 0;
-    for(i = 0; i < haystack.length; i++) {
-      if(haystack[i] === "needle") {
-        position = i;
-      }
+// function findNeedle(haystack) {
+//     let position = 0;
+//     for(i = 0; i < haystack.length; i++) {
+//       if(haystack[i] === "needle") {
+//         position = i;
+//       }
+//     }
+//     return `found the needle at position ${position}`
+// }
+////////////////////////////////////////////////////////////////////////////
+
+
+
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes 
+// into team 1, and so on.
+// Given an array of positive integers (the weights of the people), return a new 
+// array/tuple of two integers, where the first one is the total weight of team 1, 
+// and the second one is the total weight of team 2.
+// Notes:
+// Array size is at least 1.
+// All numbers will be positive.
+
+function rowWeights(array) {
+    let sum1 = 0;
+    let sum2 = 0;
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            sum1 += array[i];
+        } else if (i % 2 === 1){
+            sum2 += array[i];
+        }
     }
-    return `found the needle at position ${position}`
+    result.push(sum1, sum2);
+    return result;
 }
