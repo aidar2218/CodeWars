434d16be5241fe230fce94429e3d94dd65cbbb13
Aidar Ibraev <118173313+aidar2218@users.noreply.github.com>
2023-10-29 15:50:01 +0600

    Add files via upload

--- CodeWars/index.js
+++ CodeWars/index.js
@@ -1393,6 +1393,7 @@
 // }
 
 // console.log(disemvowel("This website is for losers LOL!"));
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // This time we want to write calculations using functions and get the results. 
@@ -1432,4 +1433,29 @@
 
 
 // console.log(nine  (dividedBy(two  ())));
-///////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////// 
+
+
+
+// You will be given a number and you will need to return it as a string in 
+// Expanded Form. For example:
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+    let multiple = 10;
+    let result = [];
+    while (num > 1) {
+        let remaninder = num % multiple;
+        if (remaninder > 0) {
+            result.unshift(remaninder);
+        }
+        num -= remaninder;
+        multiple = multiple * 10;
+    }
+    return result.join(" + ");
+}
+
+console.log(expandedForm(70304));
